507e51d7229900a8c4a8380db5e5645f2c17157c
namuan <575441+namuan@users.noreply.github.com>
2020-02-11 19:33:56 +0000

    [Added] Url Percent encoder/decoder

--- go-snippets/encoder.go
+++ go-snippets/encoder.go
@@ -1,6 +1,9 @@
 package main
 
-import "encoding/base64"
+import (
+    "encoding/base64"
+    "net/url"
+)
 
 func Base64Encode(src string) (encoded string) {
     encoded = base64.StdEncoding.EncodeToString([]byte(src))
@@ -10,5 +13,15 @@ func Base64Encode(src string) (encoded string) {
 func Base64Decode(src string) (decoded string) {
     decodedBytes, _ := base64.StdEncoding.DecodeString(src)
     decoded = string(decodedBytes)
+    return
+}
+
+func UrlEncode(src string) (encoded string) {
+    encoded = url.QueryEscape(src)
+    return
+}
+
+func UrlDecode(src string) (decoded string) {
+    decoded, _ = url.QueryUnescape(src)
     return
 }
--- go-snippets/encoder_test.go
+++ go-snippets/encoder_test.go
@@ -4,22 +4,36 @@ import "testing"
 
 func TestBase64(t *testing.T) {
 
-	t.Run("should encode string to base64", func(t *testing.T) {
-		got := Base64Encode("Hello World")
-		expected := "SGVsbG8gV29ybGQ="
+	assertEquals := func(t *testing.T, got string, expected string) {
+		t.Helper()
 
 		if got != expected {
 			t.Errorf("got %q, expected %q", got, expected)
 		}
+	}
+
+	t.Run("should encode string to base64", func(t *testing.T) {
+		assertEquals(t, Base64Encode("Hello World"), "SGVsbG8gV29ybGQ=")
 	})
 
 	t.Run("should decode string from base64", func(t *testing.T) {
-		got := Base64Decode("SGVsbG8gV29ybGQ=")
-		expected := "Hello World"
+		assertEquals(t, Base64Decode("SGVsbG8gV29ybGQ="), "Hello World")
+	})
 
-		if got != expected {
-			t.Errorf("got %q expected %q", got, expected)
-		}
+	t.Run("should encode using percent encoding", func(t *testing.T) {
+		assertEquals(
+			t,
+			UrlEncode("http://www.example.com/?a=$123"),
+			"http%3A%2F%2Fwww.example.com%2F%3Fa%3D%24123",
+		)
+	})
+
+	t.Run("should decode using percent encoding", func(t *testing.T) {
+		assertEquals(
+			t,
+			UrlDecode("http%3A%2F%2Fwww.example.com%2F%3Fa%3D%24123"),
+			"http://www.example.com/?a=$123",
+		)
 	})
 
 }
