9b5900e473139e1df8d5e537d92f7d90e688e9d8
namuan <575441+namuan@users.noreply.github.com>
2025-05-11 09:07:24 +0100

    feat: Refresh token if unable to get products due to authentication error

--- react-snippets/tanstack-query-example/src/hooks/useProduct.ts
+++ react-snippets/tanstack-query-example/src/hooks/useProduct.ts
@@ -1,23 +1,35 @@
-import { useQuery } from '@tanstack/react-query';
+import {QueryClient, useQuery, useQueryClient} from '@tanstack/react-query';
 import axios from 'axios';
-import { Product } from '../types/Product';
+import {Product} from '../types/Product';
 import {useToken} from "../context/tokenUtils.ts";
 
-const fetchProduct = async (token: string, productId: number): Promise<Product> => {
-  const { data } = await axios.get(`https://dummyjson.com/products/${productId}`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-  return data;
+const fetchProduct = async (token: string, productId: number, queryClient: QueryClient): Promise<Product> => {
+    try {
+        const {data} = await axios.get(`https://dummyjson.com/products/${productId}`, {
+            headers: {
+                Authorization: `Bearer invalid${token}`,
+            },
+        })
+        return data;
+    } catch (err) {
+        if (axios.isAxiosError(err) && err.response?.status === 401) {
+            await queryClient.invalidateQueries({queryKey: ['token']})
+            throw new Error('Authentication error, refreshing token and retrying...')
+        }
+        throw err;
+    }
 };
 
 export function useProduct() {
-  const { token } = useToken();
-  const productId = 3;
-  return useQuery({
-    queryKey: ['product', productId],
-    queryFn: () => fetchProduct(token!, productId),
-    enabled: !!token,
-  });
+    const {token} = useToken();
+    const queryClient = useQueryClient();
+    const productId = 3;
+    return useQuery({
+        queryKey: ['product', productId],
+        queryFn: () => fetchProduct(token!, productId, queryClient),
+        enabled: !!token,
+        retry: (failureCount, error) => {
+            return !!(error.message.includes('Authentication error') && failureCount < 3);
+        }
+    });
 }
